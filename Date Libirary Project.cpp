723178b4e740146bbf2ac0ad92fc628cff13d7b9
Ahmed Mohamed <aabdelaal.dev@gmail.com>
2025-10-08 12:42:03 +0300

    Add project files.

---
+++ Date Libirary Project.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include "clsDate.h"
+//#include <string>
+
+using namespace std;
+int main()
+{
+
+	//clsDate Date1("7/7/2004");
+/*	clsDate Date1(5,5,2004);
+	cout << "Date = " << Date1._Day <<"/"<<Date1._Month << "/" << Date1._Year << endl;*/	
+	clsDate Date1(300,2004);
+	clsDate Date2(32, 12, 2004);
+	//cout << "Date = " << Date1._Day <<"/"<<Date1._Month << "/" << Date1._Year << endl;
+
+	/*Date1._Day = 10;
+	Date1._Month = 12;  
+	Date1._Year = 2004;
+	cout << "Date = " << Date1._Day <<"/"<<Date1._Month << "/" << Date1._Year << endl;*/
+
+	
+
+	////cout << clsDate::DateToString(Date2) << endl;
+
+	////Date1.ShowYearCalender(2000);
+
+	//cout << Date2.IsValid() << endl;
+
+	//cout << Date1.IsDateBeforeDate2(Date2) << endl;
+
+	//cout << clsDate::IsDate1BeforeDate2(Date1, Date2) << endl;
+	//cout << clsDate::IsDate1EqualDate2(Date1, Date2) << endl;
+
+	//cout << Date2.NumOfDaysInAMonth()<<endl;
+	//cout << Date1.NumberOfDaysFromYearBegining(2004,2,1)<<endl;
+
+	//cout << Date2.IsLastDayInAMonth()<<endl;
+	//
+	// cout << clsDate::IsLastDayInMonth(31,1,2020)<<endl;
+	
+	//Date2.AddOneDay();
+
+	//Date2.Print();
+
+
+	
+
+
+
+
+
+
+	system("pause > 0");
+	return 0;
+}
